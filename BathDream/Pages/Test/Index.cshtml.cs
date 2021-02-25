4b7befaec411e150fe0ff0f9722e25ce0ed2a772
Oleg Moiseev <wizardhelg@gmail.com>
2021-02-25 15:10:56 +0300

    фиксы

--- BathDream/Pages/Test/Index.cshtml.cs
+++ BathDream/Pages/Test/Index.cshtml.cs
@@ -3,27 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BathDream.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BathDream.Pages.Test
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
 
-        public IndexModel(SignInManager<User> signInManager) => _signInManager = signInManager;
-
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; } = new InputModel();
+        public class InputModel
         {
-            
+            public string NameFamaly { get; set; }
+        }
+
+        public IndexModel(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            User user = await _userManager.FindByNameAsync(User.Identity.Name);
+            Input.NameFamaly = $"{user.UName} {user.UFamaly}";
         }
 
         public async Task<IActionResult> OnPostLogout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToPage("/Test/Index");
+            return RedirectToPage("/Index");
         }
     }
 }
