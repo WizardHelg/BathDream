4253d12985797b3f4d27ab1a7672bcc672c2c0eb
Zheltov Alexey <zheltiy86@gmail.com>
2021-05-07 15:23:49 +0300

    Страница архитектора со списком заказов

--- BathDream/Pages/Account/Architector.cshtml.cs
+++ BathDream/Pages/Account/Architector.cshtml.cs
@@ -2,15 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BathDream.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BathDream.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BathDream.Pages.Account
 {
     public class ArchitectorModel : PageModel
     {
+        public List<Order> Orders { get; set; }
+
+        private readonly DBApplicationaContext _db;
+
+        public ArchitectorModel(DBApplicationaContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+
+            Orders = _db.Orders.Where(o => o.Status == Order.Statuses.New)
+                .Include(o => o.Customer)
+                .ThenInclude(c => c.User).ToList();
         }
+
+
     }
 }
