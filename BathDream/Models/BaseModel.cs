22a435b107c24dc304a603ad22547e01dc6667a7
rv-borisov <romka_borisov@bk.ru>
2021-06-30 23:14:43 +0300

    Минорные исправления

---
+++ BathDream/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using BathDream.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BathDream.Models
+{
+    public class BaseModel
+    {
+        public BaseModel()
+        {
+            
+        }
+    }
+}
